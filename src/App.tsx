d5c04625e4ba9888597e14afc852b79a514af6dc
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-06 15:48:18 +0400

    I removed and now build new one in order to understand better

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import { useCartStore } from "./store/store";
 import "./App.css";
 
 function App() {
-  const cartCount = useCartStore((state) => state.increase);
-  const currentValue = useCartStore((state) => state.cartCount);
-  const resetButton = useCartStore((state) => state.reset);
-  return (
-    <div>
-      <h1>{currentValue}</h1>
-      <button onClick={cartCount}>Click me to increase</button>
-      <button onClick={resetButton}>reset</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
